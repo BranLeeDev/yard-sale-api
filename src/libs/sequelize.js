4db5c39872359dd260a52a8000cd4a86470fbdca
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-23 12:50:36 -0500

    Configurando sequelize para empezar a crear nuestros primeros modelo

---
+++ src/libs/sequelize.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+const { config } = require("../config/config");
+
+let URI;
+if (config.isPro) {
+  URI = config.dbUrl;
+} else {
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
+
+const sequelize = new Sequelize(URI, {
+  dialect: "postgres",
+  logging: false,
+  define: { timestamps: true },
+});
+
+module.exports = sequelize;
