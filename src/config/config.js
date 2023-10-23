b2a3538f04707358a3fbb9f5fe211ba859e29fe9
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-23 12:37:22 -0500

    Instalación de sequelize, pg y pg-hstore. Además agregamos estas variables de entorno, DATABASE_URL, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST y DB_PORT

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,14 @@ require("dotenv").config();
 
 const config = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
+  isPro: process.env.NODE_ENV === "production",
+  dbUrl: process.env.DATABASE_URL,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
 };
 
 module.exports = { config };
