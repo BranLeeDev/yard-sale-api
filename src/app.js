636f69d9c3b8cb1da00e9b30015e0f8593f430bb
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-23 12:23:23 -0500

    Instalando helmet, cors y boom, además configuramos estas librerias en nuestro 'app.js' para que funcionen correctamente

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const boom = require("@hapi/boom");
 
 const app = express();
+
+const whiteList = [];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(boom.unauthorized("You do not have permissions"));
+    }
+  },
+};
+
+app.use(helmet());
+app.use(cors(options));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("I am the HOME");
